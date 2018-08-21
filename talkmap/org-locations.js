5aae86e8edee6bd096bd402a9a994b52d02b993b
knielbo <knielbo@sdu.dk>
2018-08-21 18:29:43 +0200

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,48 +1,8 @@
 var addressPoints = [
   [
-    "Aarhus, Denmark",
-    56.1496278,
-    10.2134046
-  ],
-  [
-    "Paris, France",
-    48.85881005,
-    2.32003101155031
-  ],
-  [
-    "Helsinki",
-    60.1674086,
-    24.9425683
-  ],
-  [
-    "Copenhagen",
-    55.6867243,
-    12.5700724
-  ],
-  [
-    "Los Angeles",
-    34.0536834,
-    -118.2427669
-  ],
-  [
-    "Brussels",
-    50.8465573,
-    4.351697
-  ],
-  [
-    "Kolding",
-    55.4894999,
-    9.47905
-  ],
-  [
     "Middelfart, Denmark",
     55.4993014,
     9.7392181
-  ],
-  [
-    "Nanning, China",
-    22.832567,
-    108.3027254
   ],
   [
     "V\u00e4xj\u00f6, Sverige",
@@ -55,19 +15,14 @@ var addressPoints = [
     116.391248
   ],
   [
-    "Copenhagen, Denmark",
-    55.6867243,
-    12.5700724
+    "Baoding, China",
+    39.0915444,
+    115.335361264416
   ],
   [
-    "University of Southern Denmark, Kolding, DK",
-    55.4903842,
-    9.46486531363636
-  ],
-  [
-    "Emdrup, Denmark",
-    55.7207286,
-    12.5410549
+    "Paris, France",
+    48.85881005,
+    2.32003101155031
   ],
   [
     "Odense, Denmark",
@@ -75,8 +30,53 @@ var addressPoints = [
     10.3852104
   ],
   [
-    "Baoding, China",
-    39.0915444,
-    115.335361264416
+    "Emdrup, Denmark",
+    55.7207286,
+    12.5410549
+  ],
+  [
+    "Kolding",
+    55.4894999,
+    9.47905
+  ],
+  [
+    "Los Angeles",
+    34.0536834,
+    -118.2427669
+  ],
+  [
+    "Nanning, China",
+    22.832567,
+    108.3027254
+  ],
+  [
+    "University of Southern Denmark, Kolding, DK",
+    55.4903842,
+    9.46486531363636
+  ],
+  [
+    "Copenhagen, Denmark",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "Brussels",
+    50.8465573,
+    4.351697
+  ],
+  [
+    "Helsinki",
+    60.1674086,
+    24.9425683
+  ],
+  [
+    "Aarhus, Denmark",
+    56.1496278,
+    10.2134046
+  ],
+  [
+    "Copenhagen",
+    55.6867243,
+    12.5700724
   ]
 ];
