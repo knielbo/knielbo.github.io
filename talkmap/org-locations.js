1e7dfc3274971514578bb7b340ab6ae16a34ed56
knielbo <knielbo@sdu.dk>
2018-08-21 18:22:22 +0200

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,23 +1,13 @@
 var addressPoints = [
   [
-    "V\u00e4xj\u00f6, Sverige",
-    56.8787183,
-    14.8094385
+    "Aarhus, Denmark",
+    56.1496278,
+    10.2134046
   ],
   [
-    "Kolding",
-    55.4894999,
-    9.47905
-  ],
-  [
-    "Odense, Denmark",
-    55.3997225,
-    10.3852104
-  ],
-  [
-    "Copenhagen",
-    55.6867243,
-    12.5700724
+    "Paris, France",
+    48.85881005,
+    2.32003101155031
   ],
   [
     "Helsinki",
@@ -25,14 +15,14 @@ var addressPoints = [
     24.9425683
   ],
   [
-    "Nanning, China",
-    22.832567,
-    108.3027254
+    "Copenhagen",
+    55.6867243,
+    12.5700724
   ],
   [
     "Los Angeles",
-    34.054935,
-    -118.244476
+    34.0536834,
+    -118.2427669
   ],
   [
     "Brussels",
@@ -40,9 +30,24 @@ var addressPoints = [
     4.351697
   ],
   [
-    "Emdrup, Denmark",
-    55.7203954,
-    12.5412247
+    "Kolding",
+    55.4894999,
+    9.47905
+  ],
+  [
+    "Middelfart, Denmark",
+    55.4993014,
+    9.7392181
+  ],
+  [
+    "Nanning, China",
+    22.832567,
+    108.3027254
+  ],
+  [
+    "V\u00e4xj\u00f6, Sverige",
+    56.8787183,
+    14.8094385
   ],
   [
     "Beijing",
@@ -50,8 +55,28 @@ var addressPoints = [
     116.391248
   ],
   [
+    "Copenhagen, Denmark",
+    55.6867243,
+    12.5700724
+  ],
+  [
     "University of Southern Denmark, Kolding, DK",
     55.4903842,
     9.46486531363636
+  ],
+  [
+    "Emdrup, Denmark",
+    55.7207286,
+    12.5410549
+  ],
+  [
+    "Odense, Denmark",
+    55.3997225,
+    10.3852104
+  ],
+  [
+    "Baoding, China",
+    39.0915444,
+    115.335361264416
   ]
 ];
