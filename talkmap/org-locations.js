f405318aece59bf2ea644c73ca9c7aef3e54d1b0
knielbo <kln@cas.au.dk>
2019-08-19 07:47:16 +0200

    updates talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,23 @@
 var addressPoints = [
   [
-    "Kolding",
-    55.4894999,
-    9.47905
+    "Copenhagen",
+    55.6867243,
+    12.5700724
+  ],
+  [
+    "Nanning, China",
+    22.8211654,
+    108.3159756
+  ],
+  [
+    "Aarhus",
+    56.1496278,
+    10.2134046
+  ],
+  [
+    "Baoding, China",
+    39.09162385,
+    115.328182762847
   ],
   [
     "Odense, Denmark",
@@ -10,14 +25,14 @@ var addressPoints = [
     10.3852104
   ],
   [
-    "Lake Arrowhead",
-    34.2592869,
-    -117.185754081831
+    "Berlin",
+    52.5170365,
+    13.3888599
   ],
   [
-    "University of Copenhagen, Copenhagen, Denmark",
-    55.6801502,
-    12.5723270014063
+    "Los Angeles",
+    34.0536909,
+    -118.2427666
   ],
   [
     "Emdrup, Denmark",
@@ -25,14 +40,44 @@ var addressPoints = [
     12.5410549
   ],
   [
-    "Beijing",
-    39.906217,
-    116.3912757
+    "University of Copenhagen, Copenhagen, Denmark",
+    55.6801502,
+    12.5723270014063
+  ],
+  [
+    "University of Southern Denmark, Odense, Denmark",
+    55.3679132,
+    10.4281551582585
+  ],
+  [
+    "Kolding",
+    55.4894999,
+    9.47905
+  ],
+  [
+    "Aarhus, Denmark",
+    56.1496278,
+    10.2134046
+  ],
+  [
+    "Lake Arrowhead",
+    34.2592869,
+    -117.185754081831
   ],
   [
     "V\u00e4xj\u00f6, Sverige",
     56.8787183,
     14.8094385
+  ],
+  [
+    "Helsinki",
+    60.1674086,
+    24.9425683
+  ],
+  [
+    "Middelfart, Denmark",
+    55.4993014,
+    9.7392181
   ],
   [
     "Paris, France",
@@ -45,24 +90,14 @@ var addressPoints = [
     9.46486531363636
   ],
   [
-    "Middelfart, Denmark",
-    55.4993014,
-    9.7392181
+    "Brussels",
+    50.8465573,
+    4.351697
   ],
   [
-    "Los Angeles",
-    34.0536834,
-    -118.2427669
-  ],
-  [
-    "Aarhus, Aarhus University",
-    56.1672052,
-    10.2026088709641
-  ],
-  [
-    "Helsinki",
-    60.1674086,
-    24.9425683
+    "Beijing",
+    39.906217,
+    116.3912757
   ],
   [
     "Paris",
@@ -70,43 +105,18 @@ var addressPoints = [
     2.3514992
   ],
   [
+    "Odense",
+    55.3997225,
+    10.3852104
+  ],
+  [
+    "Aarhus, Aarhus University",
+    56.1672052,
+    10.2026088709641
+  ],
+  [
     "Copenhagen, Denmark",
     55.6867243,
     12.5700724
-  ],
-  [
-    "Copenhagen",
-    55.6867243,
-    12.5700724
-  ],
-  [
-    "Nanning, China",
-    22.8211654,
-    108.3159756
-  ],
-  [
-    "University of Southern Denmark, Odense, Denmark",
-    55.3679132,
-    10.4281551582585
-  ],
-  [
-    "Baoding, China",
-    39.0915444,
-    115.335361264416
-  ],
-  [
-    "Aarhus",
-    56.1496278,
-    10.2134046
-  ],
-  [
-    "Brussels",
-    50.8465573,
-    4.351697
-  ],
-  [
-    "Aarhus, Denmark",
-    56.1496278,
-    10.2134046
   ]
 ];
